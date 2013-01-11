013c7634b2768769bdc52c6471b1d44b61bc4098
Sean Kelly <sean.kelly@disney.com>
2013-01-11 16:03:29 -0700

    update subl packages and settings

--- kitchen/cookbooks/subl/recipes/push.rb
+++ kitchen/cookbooks/subl/recipes/push.rb
@@ -1,28 +1,35 @@
-SUBL_DIR = "#{ENV['HOME']}/bin/pkg/Sublime\\ Text\\ 2"
-if platform?("ubuntu")
-elsif platform?("mac_os_x")
-  user_dir = File.join(ENV['HOME'], 'Library', 'Application\\ Support', 'Sublime\\ Text\\ 2')
-  puts "yoda #{user_dir}"
-end
-
+require File.expand_path(File.join(File.dirname(__FILE__), 'subl'))
 
 SUBL_TAR = "subl_2.0.1.tar.gz"
 FILES_DIR = File.expand_path(File.join(File.dirname(__FILE__),'../files'))
 
-execute "tar" do
+execute "tar-app" do
   if platform?("ubuntu")
     dir = File.join(FILES_DIR, 'ubuntu')
-    command "tar cvfz #{File.join(dir,SUBL_TAR)} #{SUBL_DIR}.app #{SUBL_DIR}"
+    Subl.with_logging(cmd = "tar cvfz #{File.join(dir,SUBL_TAR)} #{SUBL_DIR}.app #{SUBL_DIR}") { command cmd }
   elsif platform?("mac_os_x")
     dir = File.join(FILES_DIR, 'mac_os_x')
-    command "tar cvfz #{File.join(dir,SUBL_TAR)} #{SUBL_DIR}.app #{user_dir}"
+    app_dir = '/Applications'
+    Subl.with_logging(cmd = "tar cvfz #{File.join(dir,SUBL_TAR)} -C #{app_dir} Sublime\\ Text\\ 2.app") {command cmd}
+  end
+  notifies :run, "execute[tar-user]"
+end
+
+execute "tar-user" do
+  if platform?("ubuntu")
+    dir = File.join(FILES_DIR, 'ubuntu')
+    Subl.with_logging(cmd = "tar cvfz #{File.join(dir,SUBL_TAR)} #{SUBL_DIR}.app #{SUBL_DIR}") { command cmd }
+  elsif platform?("mac_os_x")
+    dir = File.join(FILES_DIR, 'mac_os_x')
+    user_dir = File.join(ENV['HOME'], 'Library', 'Application\\ Support')
+    Subl.with_logging(cmd = "tar cvfz #{File.join(dir,SUBL_TAR)} -C #{user_dir} Sublime\\ Text\\ 2") {command cmd}
   end
   notifies :run, "execute[push]"
 end
 
+
 execute "push" do
-  puts 'Pushing to github'
-  command "cd #{File.dirname(__FILE__)}; git commit -a -m'update subl packages and settings'; git push"
+  Subl.with_logging(cmd = "cd #{File.dirname(__FILE__)}; git commit -a -m'update subl packages and settings'; git push") {command cmd}
   action :nothing
 end
 
