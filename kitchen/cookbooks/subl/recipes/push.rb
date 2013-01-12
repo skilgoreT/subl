3988cd32bfc4aa2ff0081e5a277a85526de90072
Sean Kelly <sean.kelly@disney.com>
2013-01-11 20:38:19 -0700

    update subl packages and settings

--- kitchen/cookbooks/subl/recipes/push.rb
+++ kitchen/cookbooks/subl/recipes/push.rb
@@ -3,19 +3,21 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'subl'))
 SUBL_TAR = "subl_2.0.1.tar"
 FILES_DIR = File.expand_path(File.join(File.dirname(__FILE__),'../files'))
 
-execute "push" do
-  if platform?("ubuntu")
 
-  elsif platform?("mac_os_x")
-    dir = File.join(FILES_DIR, 'mac_os_x')
-    app_dir = File.join('/', 'Applications')
-    Subl.with_logging(cmd = "tar cvf #{File.join(dir,SUBL_TAR)} -C #{app_dir} Sublime\\ Text\\ 2.app") {system cmd}
-    dir = File.join(FILES_DIR, 'mac_os_x')
-    user_dir = File.join(ENV['HOME'], 'Library', 'Application\\ Support')
-    Subl.with_logging(cmd = "tar rvf #{File.join(dir,SUBL_TAR)} -C #{user_dir} Sublime\\ Text\\ 2") {system cmd}
-    Subl.with_logging(cmd = "cd #{File.dirname(__FILE__)}; git commit -a -m'update subl packages and settings'; git push") {system cmd}
-  end
+if platform?("ubuntu")
+
+elsif platform?("mac_os_x")
+  dir = File.join(FILES_DIR, 'mac_os_x')
+  app_dir = File.join('/', 'Applications')
+  tarball = File.join(dir, SUBL_TAR)
+  Subl.with_logging(cmd = "tar cvf #{File.join(dir,SUBL_TAR)} -C #{app_dir} Sublime\\ Text\\ 2.app") {system cmd}
+  dir = File.join(FILES_DIR, 'mac_os_x')
+  user_dir = File.join(ENV['HOME'], 'Library', 'Application\\ Support')
+  Subl.with_logging(cmd = "tar rvf #{tarball} -C #{user_dir} Sublime\\ Text\\ 2") {system cmd}
+  Subl.with_logging(cmd = "gzip #{tarball}") {system cmd}  
+  Subl.with_logging(cmd = "cd #{File.dirname(__FILE__)}; git commit -a -m'update subl packages and settings'; git push") {system cmd}
 end
+
 
 
 
