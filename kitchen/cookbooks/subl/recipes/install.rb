22da32c08a985a6ad0e5e0ce093c18e82b01a6cf
Sean Kelly <sean.kelly@disney.com>
2013-01-13 12:26:57 -0700

    update subl packages and settings

--- kitchen/cookbooks/subl/recipes/install.rb
+++ kitchen/cookbooks/subl/recipes/install.rb
@@ -6,31 +6,31 @@
   ruby_block "install" do
    action :nothing
    block do
-     SUBL = "Sublime Text 2"
-     SUBL_APP = "Sublime Text 2.app"
-     TMP_SUBL_APP = File.join("", "tmp", "Sublime Text 2.app")
-     TMP_SUBL_USR = File.join("", "tmp", "Sublime Text 2")
-
+     Subl.with_logging(cmd = "tar xvzf #{TEMP_TARBALL} -C /tmp") {system cmd}
      if platform?("ubuntu")
+       SUBL_USR = "sublime-text-2"
+       SUBL_APP = "Sublime Text 2"
+       TMP_SUBL_APP = File.join("", "tmp", SUBL_APP)
+       TMP_SUBL_USR = File.join("", "tmp", SUBL_USR)
+       tgt = File.join(ENV['HOME'], '.config', SUBL_USR)
      elsif platform?("mac_os_x")
-       Subl.with_logging(cmd = "tar xvzf #{TEMP_TARBALL} -C /tmp") {system cmd}
-     # User settings
-     tgt = File.join(ENV['HOME'], 'Library', 'Application Support', SUBL)
+       SUBL_USR = "Sublime Text 2"
+       SUBL_APP = "Sublime Text 2.app"
+       TMP_SUBL_APP = File.join("", "tmp", SUBL_APP)
+       TMP_SUBL_USR = File.join("", "tmp", SUBL_USR)
+       tgt = File.join(ENV['HOME'], 'Library', 'Application Support', SUBL_USR)
+     end
      FileUtils.rm_rf(tgt) if Dir.exist?(tgt)    
-     FileUtils.cp_r(TMP_SUBL_USR, tgt)
-     FileUtils.chown_R ENV['USER'], 'staff', tgt
-     # Application
      tgt = File.join('', 'Applications', SUBL_APP)
      FileUtils.rm_rf(tgt) if Dir.exist?(tgt)
      FileUtils.cp_r(TMP_SUBL_APP, tgt)
      installed_app = File.join("", "Applications", SUBL_APP, "Contents", "SharedSupport", "bin", "subl")
      FileUtils.ln_sf(installed_app, File.join(ENV['HOME'], "bin", "subl"))
      Subl.with_logging( cmd = "gem install pry --no-ri --no-rdoc") { system cmd }
-    end
+   end
  end
-end
 
-cookbook_file TEMP_TARBALL do
+ cookbook_file TEMP_TARBALL do
   source SUBL_TAR
   owner `whoami`.chomp
   group `groups`.split(/\s+/)[0]
--- kitchen/cookbooks/subl/recipes/push.rb
+++ kitchen/cookbooks/subl/recipes/push.rb
@@ -3,20 +3,27 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'subl'))
 SUBL_TAR = "subl_2.0.1.tar"
 FILES_DIR = File.expand_path(File.join(File.dirname(__FILE__),'../files'))
 
-
 if platform?("ubuntu")
-
+    SUBL_USR = "sublime-text-2"
+    SUBL_APP = "Sublime Text 2"
+	tar_dir = File.join(FILES_DIR, 'ubuntu')
+	tarball = File.join(tar_dir, SUBL_TAR)
+	app_dir = File.join(ENV['HOME'], 'bin')
+	usr_dir = File.join(ENV['HOME'], '.config')
 elsif platform?("mac_os_x")
-  dir = File.join(FILES_DIR, 'mac_os_x')
-  app_dir = File.join('/', 'Applications')
-  tarball = File.join(dir, SUBL_TAR)
-  Subl.with_logging(cmd = "tar cvf #{File.join(dir,SUBL_TAR)} -C #{app_dir} Sublime\\ Text\\ 2.app") {system cmd}
-  dir = File.join(FILES_DIR, 'mac_os_x')
-  user_dir = File.join(ENV['HOME'], 'Library', 'Application\\ Support')
-  Subl.with_logging(cmd = "tar rvf #{tarball} -C #{user_dir} Sublime\\ Text\\ 2") {system cmd}
-  Subl.with_logging(cmd = "gzip -f #{tarball}") {system cmd}  
-  Subl.with_logging(cmd = "cd #{File.dirname(__FILE__)}; git commit -a -m'update subl packages and settings'; git push") {system cmd}
+    SUBL_USR = "Sublime Text 2"
+    SUBL_APP = "Sublime Text 2.app"
+	tar_dir = File.join(FILES_DIR, 'mac_os_x')
+	tarball = File.join(tar_dir, SUBL_TAR)
+	usr_dir = File.join(ENV['HOME'], 'Library', 'Application\\ Support')
+	app_dir = File.join('', 'Applications')
 end
+
+Subl.with_logging(cmd = "tar cvf #{File.join(tar_dir,SUBL_TAR)} -C #{app_dir} #{SUBL_APP}") {system cmd}
+Subl.with_logging(cmd = "tar rvf #{tarball} -C #{usr_dir} #{SUBL_USR}") {system cmd}
+Subl.with_logging(cmd = "gzip -f #{tarball}") {system cmd}  
+Subl.with_logging(cmd = "cd #{File.dirname(__FILE__)}; git commit -a -m'update subl packages and settings'; git push") {system cmd}
+
 
 
 
