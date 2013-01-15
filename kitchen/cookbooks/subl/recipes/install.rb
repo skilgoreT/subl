0c835b6379145d5f568835737eaa8d28a5d8efc8
Sean Kelly <sean.kelly@disney.com>
2013-01-14 21:50:00 -0700

    handle space escape in filename

--- kitchen/cookbooks/subl/recipes/install.rb
+++ kitchen/cookbooks/subl/recipes/install.rb
@@ -1,50 +1,48 @@
   require File.expand_path(File.join(File.dirname(__FILE__), 'subl'))
 
-  TEMP_TARBALL = "/tmp/subl_2.0.1.tgz"
-  TEMP_USR_TARBALL = "/tmp/subl_usr.tgz"
   SUBL_TAR = "subl_2.0.1.tgz"
   SUBL_USR_TAR = "subl_usr.tgz"
-  
+  FILES_DIR = File.expand_path(File.join(File.dirname(__FILE__),'../files'))
+  USR_TARBALL = File.join(FILES_DIR, 'default', SUBL_USR_TAR)
+  TEMP_TARBALL = "/tmp/#{SUBL_TAR}"
+
   ruby_block "install" do
    action :nothing
    block do
      Subl.with_logging(cmd = "tar xvzf #{TEMP_TARBALL} -C /tmp") {system cmd}
      if platform?("ubuntu")
-       SUBL_USR = "sublime-text-2"
+       # App
        SUBL_APP = "Sublime Text 2"
        TMP_SUBL_APP = File.join("", "tmp", SUBL_APP)
-       TMP_SUBL_USR = File.join("", "tmp", SUBL_USR)
        APP_TGT = File.join(ENV['HOME'], 'bin', SUBL_APP)
-       USR_TGT = File.join(ENV['HOME'], '.config', SUBL_USR)
        APP = File.join(APP_TGT,'sublime_text')
+       # User Data
+       USR_TGT = File.join(ENV['HOME'], '.config', 'sublime-text-2')
      elsif platform?("mac_os_x")
-       SUBL_USR = "Sublime Text 2"
+       # App
        SUBL_APP = "Sublime Text 2.app"
        TMP_SUBL_APP = File.join("", "tmp", SUBL_APP)
-       TMP_SUBL_USR = File.join("", "tmp", SUBL_USR)
        APP_TGT = File.join('', 'Applications', SUBL_APP)
-       USR_TGT = File.join(ENV['HOME'], 'Library', 'Application Support', SUBL_USR)
        APP = File.join("", "Applications", SUBL_APP, "Contents", "SharedSupport", "bin", "subl")
+       # User Data
+       USR_TGT = File.join(ENV['HOME'], 'Library', 'Application Support', 'Sublime Text 2')
      end
+     # User Data
+     FileUtils.rm_rf(File.join(USR_TGT)) if Dir.exist?(USR_TGT)
+     FileUtils.mkdir(USR_TGT)
+     # Need to escape spaces in string before feeding to TAR
+     target = USR_TGT.gsub(/(\s)/, "\\ ")
+     Subl.with_logging(cmd = "tar xvzf #{USR_TARBALL} -C #{target} .") {system cmd}
+     # App
      FileUtils.rm_rf(APP_TGT) if Dir.exist?(APP_TGT)    
      FileUtils.cp_r(TMP_SUBL_APP, APP_TGT)
-     FileUtils.rm_rf(USR_TGT) if Dir.exist?(USR_TGT)    
-     FileUtils.cp_r(TMP_SUBL_USR, USR_TGT)
      FileUtils.ln_sf(APP, File.join(ENV['HOME'], "bin", "subl"))
      # Ruby support for SublimeREPL
      Subl.with_logging( cmd = "gem install pry --no-ri --no-rdoc") { system cmd }
    end
  end
 
- cookbook_file TEMP_TARBALL do
-  source SUBL_TAR
-  owner `whoami`.chomp
-  group `groups`.split(/\s+/)[0]
-  mode "0644"
-  notifies :create, resources(:ruby_block => "install")
-end
-
-cookbook_file TEMP_USR_TARBALL do
+cookbook_file TEMP_TARBALL do
   source SUBL_TAR
   owner `whoami`.chomp
   group `groups`.split(/\s+/)[0]
