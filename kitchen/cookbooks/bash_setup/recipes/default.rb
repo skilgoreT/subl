d18005ffeda88dbef7019ac9e64442aec8215076
skelly <skelly@kilaua.(none)>
2013-03-04 20:37:46 -0700

    template file

--- kitchen/cookbooks/bash_setup/recipes/default.rb
+++ kitchen/cookbooks/bash_setup/recipes/default.rb
@@ -1,20 +1,20 @@
 cookbook_file File.join(ENV['HOME'], '.bash_profile') do
   owner ENV['SUDO_USER']
-  group 'staff'
+  group `groups`.split(/\s/)[0]
   mode 0644
   source "bash_profile"
 end
 
 cookbook_file File.join(ENV['HOME'], '.bash_profile') do
   owner ENV['SUDO_USER']
-  group 'staff'
+  group `groups`.split(/\s/)[0]
   mode 0644
   source "bash_profile"
 end
 
 cookbook_file File.join(ENV['HOME'], '.bash_aliases') do
   owner ENV['SUDO_USER']
-  group 'staff'
+  group `groups`.split(/\s/)[0]
   mode 0644
   source "bash_aliases"
 end
@@ -22,7 +22,7 @@ end
 remote_directory File.join(ENV['HOME'], 'bin') do
   files_backup 0
   files_owner ENV['SUDO_USER']
-  files_group 'staff'
+  files_group `groups`.split(/\s/)[0]
   files_mode 0755
   source "bin"
 end
